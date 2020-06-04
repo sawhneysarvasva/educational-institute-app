f4f82a278fa436f3d504b34cee288ab2261ef912
Admin <Admin@Dell>
2020-06-04 23:55:33 +0530

    Initial Project Commit

---
+++ educational-institute-app/src/main/java/com/sarvasva/educationalinstituteapp/EducationalInstituteAppApplication.java
@@ -0,0 +1,13 @@
+package com.sarvasva.educationalinstituteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalInstituteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalInstituteAppApplication.class, args);
+	}
+
+}
---
+++ educational-institute-app/src/test/java/com/sarvasva/educationalinstituteapp/EducationalInstituteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarvasva.educationalinstituteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalInstituteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
